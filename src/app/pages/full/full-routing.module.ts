8ed6a6aaa1fb3f1e60d6c5a76708e0c37b1a17e6
Carlos Torres <carlostorres8791@gmail.com>
2022-11-01 17:45:40 -0500

    merge commits

--- src/app/pages/full/full-routing.module.ts
+++ src/app/pages/full/full-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ServicesComponent } from './services/services.component';
 
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: "profile", component: ProfileComponent },
   { path: "contact", component: ContactComponent },
   { path: "services", component: ServicesComponent },
+  { path: "product", component: ProductsComponent}
 ];
 
 @NgModule({
