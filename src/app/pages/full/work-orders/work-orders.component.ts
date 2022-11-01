cbb2f84dc097e51008d863e976ce8956f91b59b2
Carlos Torres <carlostorres8791@gmail.com>
2022-11-01 09:58:04 -0500

    create components

---
+++ src/app/pages/full/work-orders/work-orders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-work-orders',
+  templateUrl: './work-orders.component.html',
+  styleUrls: ['./work-orders.component.sass']
+})
+export class WorkOrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
